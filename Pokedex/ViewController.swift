66d6d44077f0c4682a3d64bccbd8e09ce6f301ad
Karen Natalia <karen.natalia888@gmail.com>
2023-03-24 11:49:38 +0700

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Karen Natalia on 24/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
